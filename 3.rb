b012446183a028425e701ec4d60293a119971d0c
Chris Lundquist <chris.lundquist@github.com>
2014-11-13 12:16:08 -0800

    [3] clean and comment

--- 3.rb
+++ 3.rb
@@ -2,6 +2,7 @@
 
 target = ARGV.shift || '1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736'
 
+# Make a Hash of a bunch of words to analyze results
 words = Hash.new
 File.open("/usr/share/dict/words") do |file|
   file.each_line do |line|
@@ -11,19 +12,20 @@ end
 
 puts "Loaded #{words.keys.length} words"
 
+# Turn our ascii string into cipher_text
 ascii_target = [target].pack("H*")
 
-results = (0..255).map do |i|
-  ascii_target.each_byte.map { |byte| (byte ^ i).chr }.join
+# Generate a table of key -> result
+# A given is that the key was one byte
+results = (0..255).each_with_object(Hash.new) do |i,table|
+  table[i] = ascii_target.each_byte.map { |byte| (byte ^ i).chr }.join
 end
 
-likely_clear_text = results.max_by do |result|
+# The best result has the most matching words delimited by spaces
+key, likely_clear_text = results.max_by do |key,result|
   tokens = result.split(" ")
   tokens.count { |token| words[token] }
 end
 
-key = results.find_index(likely_clear_text)
-
 puts "Key: #{key}"
 puts "Clear Text: #{likely_clear_text}"
-
