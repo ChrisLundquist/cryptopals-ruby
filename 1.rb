f4441e33bc4b1f485c4d359eb346559509956ceb
Chris Lundquist <chris.lundquist@github.com>
2014-11-13 17:22:35 -0800

    [1] make it work by default

--- 1.rb
+++ 1.rb
@@ -2,7 +2,7 @@
 
 require 'base64'
 
-data = STDIN.read or ARGV
+data = ARGV.shift || "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
 
 ascii = [data].pack('H*')
 # Challenge text: "I'm killing your brain like a poisonous mushroom"
